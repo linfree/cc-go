51a6b26420fed279383b629e979075ff21f22122
linfree <linfree.php@outlook.com>
2026-05-12 11:35:28 +0800

    feat(ui): add Linux/Mac systray implementation

---
+++ internal/ui/ui_other.go
@@ -0,0 +1,63 @@
+//go:build !windows
+
+package ui
+
+import (
+	"fmt"
+
+	"github.com/getlantern/systray"
+)
+
+type otherUI struct {
+	port  int
+	icon  []byte
+	quitC chan struct{}
+}
+
+func newPlatformUI(port int, icon []byte) UI {
+	return &otherUI{port: port, icon: icon, quitC: make(chan struct{})}
+}
+
+func (u *otherUI) Run(onReady func()) {
+	systray.Run(func() {
+		systray.SetIcon(u.icon)
+		systray.SetTitle("cc-go")
+		systray.SetTooltip("cc-go - Claude Code remote manager")
+
+		mOpen := systray.AddMenuItem("打开 Web 管理面板", "在浏览器中打开管理面板")
+		systray.AddSeparator()
+		mQuit := systray.AddMenuItem("退出", "停止服务并退出")
+
+		// Auto-open browser on start
+		url := fmt.Sprintf("http://localhost:%d", u.port)
+		openBrowser(url)
+
+		if onReady != nil {
+			onReady()
+		}
+
+		go func() {
+			for {
+				select {
+				case <-mOpen.ClickedCh:
+					openBrowser(url)
+				case <-mQuit.ClickedCh:
+					systray.Quit()
+					return
+				case <-u.quitC:
+					systray.Quit()
+					return
+				}
+			}
+		}()
+	}, func() {
+		// onExit — nothing extra needed
+	})
+}
+
+func (u *otherUI) Quit() {
+	select {
+	case u.quitC <- struct{}{}:
+	default:
+	}
+}
